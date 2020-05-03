b2e7a23d1083a4a1e11e17ca21aa4011d4f4fa43
Matthew Berry <me@mattrb.com>
2020-05-02 23:07:05 -0700

    initial commit

---
+++ spec/gb_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Gb do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/gb.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Gb`
+module Gb
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
