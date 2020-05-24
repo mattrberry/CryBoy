04098b67332f69a969eb8474a3edcfeb8b96daca
Matthew Berry <me@mattrb.com>
2020-05-23 23:41:07 -0700

    smaller default display size

--- src/cryboy/display.cr
+++ src/cryboy/display.cr
@@ -3,7 +3,7 @@ require "sdl"
 class Display
   @colors = [SDL::Color[0xFF], SDL::Color[0xAA], SDL::Color[0x55], SDL::Color[0x00]]
 
-  def initialize(@scale = 4, @width = 160, @height = 144)
+  def initialize(@scale = 2, @width = 160, @height = 144)
     SDL.init(SDL::Init::VIDEO)
     at_exit { SDL.quit }
     @window = SDL::Window.new("CryBoy", @width * @scale, @height * @scale)
