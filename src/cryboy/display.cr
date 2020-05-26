fc6e6951ca4226ae77b1de185f081c99769e402d
Matthew Berry <me@mattrb.com>
2020-05-25 22:56:45 -0700

    first step towards trying to implement sprites. doesnt work.

--- src/cryboy/display.cr
+++ src/cryboy/display.cr
@@ -8,41 +8,6 @@ class Display
     at_exit { SDL.quit }
     @window = SDL::Window.new("CryBoy", @width * @scale, @height * @scale)
     @renderer = SDL::Renderer.new @window
-    # @all_tiles_window = SDL::Window.new("ALL TILES", 256 * @scale, 256 * @scale)
-    # @all_tiles_renderer = SDL::Renderer.new @all_tiles_window
-  end
-
-  def draw_scanlines : Nil
-    @renderer.draw_color = SDL::Color[0xFF, 0x00, 0x00]
-    (1...@width).each do |col|
-      @renderer.draw_line col * @scale, 0, col * @scale, @height * @scale
-    end
-    (1...@height).each do |row|
-      @renderer.draw_line 0, row * @scale, @width * @scale, row * @scale
-    end
-  end
-
-  # a method for showing all tiles in vram at 0x8000 (for debugging)
-  def draw_all_tiles(memory : Memory, scanlines : Array(Scanline))
-    (0...32).each do |y|
-      (0...32).each do |x|
-        tile_ptr = 0x8000 + (y * 16 * 16) + (x * 16)
-        # puts "tile ptr: #{hex_str tile_ptr.to_u16}"
-        (0...8).each do |tile_row|
-          byte_1 = memory[tile_ptr + 2 * tile_row]
-          byte_2 = memory[tile_ptr + 2 * tile_row + 1]
-          (0...8).each do |tile_col|
-            lsb = (byte_1 >> (7 - tile_col)) & 0x1
-            msb = (byte_2 >> (7 - tile_col)) & 0x1
-            @all_tiles_renderer.draw_color = @colors[(msb << 1) | lsb]
-            # puts "x:#{8 * x + tile_col},\ty:#{8 * y + tile_row},\tmsb:#{msb},\tlsb:#{lsb},\tcom:#{(msb << 1) | lsb}, color:#{@colors[(msb << 1) | lsb]}"
-            # @all_tiles_renderer.fill_rect((8 * x + tile_col) * @scale, (8 * y + tile_row) * @scale, @scale, @scale)
-            @all_tiles_renderer.fill_rect((8 * (x % 16) + tile_col) * @scale, (8 * (y + (x // 16)) + tile_row) * @scale, @scale, @scale)
-          end
-        end
-      end
-    end
-    @all_tiles_renderer.present
   end
 
   def draw(framebuffer : Array(Array(UInt8)), palette : UInt8) : Nil
@@ -53,8 +18,6 @@ class Display
         @renderer.fill_rect(x * @scale, y * @scale, @scale, @scale)
       end
     end
-
-    # draw_scanlines
     @renderer.present
   end
 end
--- src/cryboy/ppu.cr
+++ src/cryboy/ppu.cr
@@ -9,6 +9,47 @@ struct Scanline
   end
 end
 
+struct Sprite
+  def initialize(@y : UInt8, @x : UInt8, @tile_num : UInt8, @attributes : UInt8)
+  end
+
+  def to_s(io : IO)
+    io << "Sprite(y:#{@y}, x:#{@x}, tile_num:#{@tile_num}, tile_ptr: #{hex_str tile_ptr}, visible:#{visible?}, priority:#{priority}, y_flip:#{y_flip?}, x_flip:#{x_flip?}, palette_number:#{palette_number}"
+  end
+
+  def visible? : Bool
+    ((1...160).includes? y) && ((1...168).includes? x)
+  end
+
+  def y : UInt8
+    @y #- 16
+  end
+
+  def x : UInt8
+    @x #- 8
+  end
+
+  def tile_ptr : UInt16
+    0x8000_u16 + 16 * @tile_num
+  end
+
+  def priority : UInt8
+    (@attributes >> 7) & 0x1
+  end
+
+  def y_flip? : Bool
+    (@attributes >> 6) & 0x1 == 1
+  end
+
+  def x_flip? : Bool
+    (@attributes >> 5) & 0x1 == 1
+  end
+
+  def palette_number : UInt8
+    (@attributes >> 4) & 0x1
+  end
+end
+
 class PPU
   property scanlines
 
@@ -21,26 +62,53 @@ class PPU
     @scanlines[y] = Scanline.new scx, scy, wx, wy, bg_window_tile_map
   end
 
-  def framebuffer : Array(Array(UInt8))
+  def draw_background(x : Int, y : Int, scanline : Scanline)
     background_map = bg_tile_map == 0_u8 ? 0x9800 : 0x9C00
+    tile_num = @memory[background_map + (((x + scanline.scx) // 8) % 32) + ((((y + scanline.scy) // 8) * 32) % (32 * 32))]
+    tile_data_table = scanline.tile_map == 0 ? 0x9000 : 0x8000
+    tile_ptr = tile_data_table + 16 * tile_num
+    tile_row = (y + scanline.scy) % 8
+    byte_1 = @memory[tile_ptr + tile_row * 2]
+    byte_2 = @memory[tile_ptr + tile_row * 2 + 1]
+    lsb = (byte_1 >> (7 - ((x + scanline.scx) % 8))) & 0x1
+    msb = (byte_2 >> (7 - ((x + scanline.scx) % 8))) & 0x1
+    color = (msb << 1) | lsb
+    @framebuffer[y][x] = color
+  end
+
+  def draw_sprites
+    # puts "drawing sprites"
+    (0xFF00..0xFF9F).step 4 do |sprite_address|
+      sprite = Sprite.new @memory[sprite_address], @memory[sprite_address + 1], @memory[sprite_address + 2], @memory[sprite_address + 3]
+      puts sprite if sprite.visible?
+      if sprite.visible?
+        (0...8).each do |row|
+          break if sprite.y + row < 16
+          byte_1 = @memory[sprite.tile_ptr + row * 2]
+          byte_2 = @memory[sprite.tile_ptr + row * 2 + 1]
+          (0...8).each do |col|
+            break if sprite.x + col < 8
+            lsb = (byte_1 >> (7 - col)) & 0x1
+            msb = (byte_2 >> (7 - col)) & 0x1
+            color = (msb << 1) | lsb
+            @framebuffer[sprite.y - 16][sprite.x - 8] = color
+          end
+        end
+      end
+    end
+  end
+
+  def framebuffer : Array(Array(UInt8))
     @scanlines.each_with_index do |scanline, y|
       (0...160).each do |x|
         if window_enabled? && scanline.wy <= y && scanline.wx <= x
           puts "window enabled"
         elsif bg_display?
-          tile_num = @memory[background_map + (((x + scanline.scx) // 8) % 32) + ((((y + scanline.scy) // 8) * 32) % (32 * 32))]
-          tile_data_table = scanline.tile_map == 0 ? 0x9000 : 0x8000
-          tile_ptr = tile_data_table + 16 * tile_num
-          tile_row = (y + scanline.scy) % 8
-          byte_1 = @memory[tile_ptr + tile_row * 2]
-          byte_2 = @memory[tile_ptr + tile_row * 2 + 1]
-          lsb = (byte_1 >> (7 - ((x + scanline.scx) % 8))) & 0x1
-          msb = (byte_2 >> (7 - ((x + scanline.scx) % 8))) & 0x1
-          color = (msb << 1) | lsb
-          @framebuffer[y][x] = color
+          draw_background x, y, scanline
         end
       end
     end
+    # draw_sprites if sprite_enabled?
     @framebuffer
   end
 
