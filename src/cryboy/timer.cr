65553f2a6cd24a56c59ca5ebb52d6162431bd919
Matthew Berry <me@mattrb.com>
2020-06-23 14:54:28 -0700

    now passing all mooneye timer tests
    fixed tma_write_reloading test

--- src/cryboy/timer.cr
+++ src/cryboy/timer.cr
@@ -6,9 +6,8 @@ class Timer
   @clock_select : UInt8 = 0x00 # frequency flag determining when to increment tima
   @bit_for_tima = 9            # bit to detect falling edge for tima increments
 
-  @previous = false
-
-  @countdown = -1
+  @previous_bit = false # used to detect falling edge
+  @countdown = -1       # load tma and set interrupt flag when countdown is 0
 
   def initialize(@interrupts : Interrupts)
   end
@@ -16,27 +15,22 @@ class Timer
   # tick timer forward by specified number of cycles
   def tick(cycles : Int) : Nil
     while cycles > 0
-      # puts "===== incrementing cycle. tima: #{@tima}"
-      # puts "ticking div from #{hex_str @div} to #{hex_str (@div &+ 1)}, countdown: #{@countdown}"
       cycles -= 1
 
       @countdown -= 1 if @countdown > -1
-      # puts "decrementing countdown to #{@countdown - 1}" if @countdown > -1
       if @countdown == 0
-        # puts "loading tma and setting interrupt flag"
         @interrupts.timer_interrupt = true
         @tima = @tma
       end
 
       @div &+= 1
-      current = @enabled && (@div & (1 << @bit_for_tima) != 0)
-      if @previous && !current
+      current_bit = @enabled && (@div & (1 << @bit_for_tima) != 0)
+      if @previous_bit && !current_bit
         @tima &+= 1
-        # puts "setting countdown to 4" if @tima == 0
         @countdown = 4 if @tima == 0
       end
-      @previous = current
-      # puts "----- done incrementing cycle. tima: #{@tima}"
+
+      @previous_bit = current_bit
     end
   end
 
@@ -54,17 +48,15 @@ class Timer
   # write to timer memory
   def []=(index : Int, value : UInt8) : Nil
     case index
-    when 0xFF04
-      @div = 0x0000_u16
+    when 0xFF04 then @div = 0x0000_u16
     when 0xFF05
-      # puts "TIMA: #{hex_str value}, countdown: #{@countdown}"
       if @countdown != 0 # ignore writes on cycle that tma is loaded
         @tima = value
         @countdown = -1 # abort interrupt and tma load
       end
     when 0xFF06
-      # puts "TMA: #{hex_str value}, countdown: #{@countdown}"
       @tma = value
+      @tima = @tma if @countdown == 0 # write to tima on cycle that tma is loaded
     when 0xFF07
       @enabled = value & 0b100 != 0
       @clock_select = value & 0b011
