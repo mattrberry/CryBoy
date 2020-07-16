9e83b9bdf637649da36458f75fe8140691bb43f2
Matthew Berry <me@mattrb.com>
2020-07-16 11:00:13 -0700

    allow tests to at least build

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,17 +2,30 @@ require "spec"
 require "../src/cryboy"
 
 # define a new CPU with the given bytes as rom
-def new_cpu(bytes : Array(Int))
-  cpu = CPU.new(new_memory(bytes), Timer.new, true)
+def new_cpu(bytes : Array(Int), cgb_enabled = true, boot = false)
+  interrupts = Interrupts.new
+  display = Display.new
+  ppu = PPU.new display, interrupts, pointerof(cgb_enabled)
+  apu = APU.new
+  timer = Timer.new interrupts
+
+  cpu = CPU.new new_memory(bytes), interrupts, ppu, apu, timer, boot
   cpu.sp = 0xFFFE_u16
   cpu
 end
 
 # define a new Memory with the given bytes as rom
-def new_memory(bytes : Array(Int))
-  rom = Bytes.new bytes.size
+def new_memory(bytes : Array(Int), cgb_enabled = true, bootrom = nil)
+  rom = Bytes.new 0x8000
   bytes.each_with_index do |byte, i|
     rom[i] = byte.to_u8!
   end
-  Memory.new(Cartridge.new(rom), Joypad.new, Timer.new, APU.new)
+  cartridge = Cartridge.new rom
+  interrupts = Interrupts.new
+  display = Display.new
+  ppu = PPU.new display, interrupts, pointerof(cgb_enabled)
+  apu = APU.new
+  timer = Timer.new interrupts
+  joypad = Joypad.new
+  Memory.new cartridge, interrupts, ppu, apu, timer, joypad, pointerof(cgb_enabled), bootrom
 end
