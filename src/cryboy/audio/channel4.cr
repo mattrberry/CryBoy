fe45c687a3c3d155e00809bc478156285b4df100
Matthew Berry <me@mattrb.com>
2020-07-19 22:44:05 -0700

    channel 4 rewrite, passing blargg's 03-trigger.gb

--- src/cryboy/audio/channel4.cr
+++ src/cryboy/audio/channel4.cr
@@ -1,67 +1,143 @@
-class Channel4 < VolumeEnvelopeChannel
-  @@RANGE = 0xFF20..0xFF23
+class Channel4
+  RANGE = 0xFF20..0xFF23
+
+  def ===(value) : Bool
+    value.is_a?(Int) && RANGE.includes?(value)
+  end
+
+  property enabled : Bool = false
+  @dac_enabled : Bool = false
 
   @lfsr : UInt16 = 0x0000
 
-  @remaining_length : UInt8 = 0x00
+  # NR41
+  @length_load : UInt8 = 0x00
 
-  @shift_clock_frequency : UInt8 = 0x00
-  @counter_step : UInt8 = 0x00
-  @dividing_ratio : UInt8 = 0x00
+  @length_counter : UInt8 = 0x00
+  @cycles_since_length_step : UInt16 = 0x0000
 
-  @counter_selection : Bool = true
+  # NR42
+  @starting_volume : UInt8 = 0x00
+  @envelope_add_mode : Bool = false
+  @period : UInt8 = 0x00
 
-  def reload_period : Nil
-    @period = (@dividing_ratio == 0 ? 8 : 16 * @dividing_ratio) << @shift_clock_frequency
+  @volume_envelope_timer : UInt8 = 0x00
+  @current_volume : UInt8 = 0x00
+
+  # NR43
+  @clock_shift : UInt8 = 0x00
+  @width_mode : UInt8 = 0x00
+  @divisor_code : UInt8 = 0x00
+
+  @frequency_timer : UInt32 = 0x00000000
+
+  # NR44
+  @length_enable : Bool = false
+
+  def step : Nil
+    if @frequency_timer == 0
+      @frequency_timer = (@divisor_code == 0 ? 8_u32 : @divisor_code.to_u32 << 4) << @clock_shift
+      new_bit = (@lfsr & 0b01) ^ ((@lfsr & 0b10) >> 1)
+      @lfsr >>= 1
+      @lfsr |= new_bit << 14
+      if @width_mode != 0
+        @lfsr &= ~(1 << 6)
+        @lfsr |= new_bit << 6
+      end
+    end
+    @frequency_timer -= 1
+    # Update frame sequencer counters
+    @cycles_since_length_step += 1
   end
 
-  def step_wave_generation : Nil
-    new_bit = (@lfsr & 0b01) ^ ((@lfsr & 0b10) >> 1)
-    @lfsr >>= 1
-    @lfsr |= new_bit << 14
-    if @counter_step != 0
-      @lfsr &= ~(1 << 6)
-      @lfsr |= new_bit << 6
+  def length_step : Nil
+    if @length_enable && @length_counter > 0
+      @length_counter -= 1
+      @enabled = false if @length_counter == 0
+    end
+    @cycles_since_length_step = 0
+  end
+
+  def volume_step : Nil
+    if @period != 0
+      @volume_envelope_timer -= 1 if @volume_envelope_timer > 0
+      if @volume_envelope_timer == 0
+        @volume_envelope_timer = @period
+        if (@current_volume < 0xF && @envelope_add_mode) || (@current_volume > 0 && !@envelope_add_mode)
+          @current_volume += (@envelope_add_mode ? 1 : -1)
+        end
+      end
     end
   end
 
   def get_amplitude : Float32
-    if @dac_enabled
-      (~@lfsr & 0x1).to_f32 * @volume / 15
+    if @enabled && @dac_enabled
+      dac_input = (~@lfsr & 1) * @current_volume
+      dac_output = (dac_input / 7.5) - 1
+      dac_output
     else
-      0_f32
-    end
+      0
+    end.to_f32
   end
 
   def [](index : Int) : UInt8
     case index
-    when 0xFF20 then 0xFF_u8 # I assume this is write-only like in the tone channels, although the pandocs say differently
-    when 0xFF21 then self.volume_envelope
-    when 0xFF22 then (@shift_clock_frequency << 4) | (@counter_step << 3) | @dividing_ratio
-    when 0xFF23 then 0xBF_u8 | (@counter_selection ? 0x40 : 0)
-    else             raise "Reading from invalid channel 4 register: #{hex_str index.to_u16!}"
-    end
+    when 0xFF20 then 0xFF
+    when 0xFF21 then @starting_volume << 4 | (@envelope_add_mode ? 0x08 : 0) | @period
+    when 0xFF22 then @clock_shift << 4 | @width_mode << 3 | @divisor_code
+    when 0xFF23 then 0xBF | (@length_enable ? 0x40 : 0)
+    else             raise "Reading from invalid Channel4 register: #{hex_str index.to_u16}"
+    end.to_u8
   end
 
   def []=(index : Int, value : UInt8) : Nil
     case index
-    when 0xFF20 then @remaining_length = 64_u8 - (value & 0x3F)
-    when 0xFF21 then self.volume_envelope = value
+    when 0xFF20
+      @length_load = value & 0x3F
+      # Internal values
+      @length_counter = 0x40_u8 - @length_load
+    when 0xFF21
+      @starting_volume = value >> 4
+      @envelope_add_mode = value & 0x08 > 0
+      @period = value & 0x07
+      # Internal values
+      @dac_enabled = value & 0xF8 > 0
+      @enabled = false if !@dac_enabled
     when 0xFF22
-      @shift_clock_frequency = value >> 4
-      @counter_step = (value >> 3) & 0x1
-      @dividing_ratio = value & 0x7
+      @clock_shift = value >> 4
+      @width_mode = (value & 0x08) >> 3
+      @divisor_code = value & 0x07
     when 0xFF23
-      @counter_selection = value & 0x40 != 0
-      trigger = value & (0x1 << 7) != 0
-      if trigger
-        @enabled = true
-        @remaining_length = 64 if @remaining_length == 0
-        reload_period
-        reset_volume_envelope
-        @lfsr = 0x7FFF_u16
+      length_enable = value & 0x40 > 0
+      # Obscure length counter behavior #1
+      if @cycles_since_length_step < 2 ** 13 && !@length_enable && length_enable && @length_counter > 0
+        @length_counter -= 1
+        @enabled = false if @length_counter == 0
       end
-    else raise "Writing to invalid channel 4 register: #{hex_str index.to_u16!}"
+      @length_enable = length_enable
+      trigger = value & 0x80 > 0
+      if trigger
+        puts "triggered"
+        puts "  NR41: length_load:#{@length_load}"
+        puts "  NR42: starting_volume:#{@starting_volume}, envelope_add_mode:#{@envelope_add_mode}, period:#{@period}"
+        puts "  NR43: clock_shift:#{@clock_shift}, width_mode:#{@width_mode}, divisor_code:#{@divisor_code}"
+        puts "  NR44: length_enable:#{@length_enable}"
+        @enabled = true if @dac_enabled
+        # Init length
+        if @length_counter == 0
+          @length_counter = 0x40
+          # Obscure length counter behavior #2
+          @length_counter -= 1 if @length_enable && @cycles_since_length_step < 2 ** 13
+        end
+        # Init frequency
+        @frequency_timer = (@divisor_code == 0 ? 8_u32 : @divisor_code.to_u32 << 4) << @clock_shift
+        # Init volume envelope
+        @volume_envelope_timer = @period
+        @current_volume = @starting_volume
+        # Init lfsr
+        @lfsr = 0x7FFF
+      end
+    else raise "Writing to invalid Channel4 register: #{hex_str index.to_u16}"
     end
   end
 end
