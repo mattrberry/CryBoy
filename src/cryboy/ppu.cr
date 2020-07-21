feb02399b3a3014543719edbf9b532b62ba3a6b1
Matthew Berry <me@mattrb.com>
2020-07-21 14:44:07 -0700

    a step into fifo, background sort of renders now

--- src/cryboy/ppu.cr
+++ src/cryboy/ppu.cr
@@ -87,8 +87,23 @@ struct RGB
   end
 end
 
+struct Pixel
+  property color : UInt8               # 0-3
+  property palette : UInt8             # 0-7
+  property sprite_priority : UInt8     # OAM index for sprite
+  property background_priority : UInt8 # OBJ-to_BG Priority bit
+
+  def initialize(@color : UInt8, @palette : UInt8, @sprite_priority : UInt8, @background_priority : UInt8)
+  end
+end
+
 class PPU
   @framebuffer = Array(RGB).new Display::WIDTH * Display::HEIGHT, RGB.new(0, 0, 0)
+  @framebuffer_pos = 0
+
+  @fifo = Deque(Pixel).new 8
+
+  @cycle_counter : UInt16 = 0x0000 # count number of cycles into current line
 
   @palettes = Array(Array(RGB)).new 8 { Array(RGB).new 4, RGB.new(0, 0, 0) }
   @palette_index : UInt8 = 0
@@ -97,8 +112,6 @@ class PPU
   @obj_palettes = Array(Array(RGB)).new 8 { Array(RGB).new 4, RGB.new(0, 0, 0) }
   @obj_palette_index : UInt8 = 0
   @obj_auto_increment = false
-
-  @counter : UInt32 = 0_u32
 
   @vram = Array(Bytes).new 2 { Bytes.new Memory::VRAM.size } # 0x8000..0x9FFF
   @vram_bank : UInt8 = 0                                     # track which bank is active
@@ -137,133 +150,6 @@ class PPU
     ] if !@cgb_ptr.value
   end
 
-  # get first 10 sprites on scanline, ordered
-  # the order dictates how sprites should render, with the first ones on the bottom
-  def get_sprites : Array(Sprite)
-    sprites = [] of Sprite
-    (0x00..0x9F).step 4 do |sprite_address|
-      sprite = Sprite.new @sprite_table[sprite_address], @sprite_table[sprite_address + 1], @sprite_table[sprite_address + 2], @sprite_table[sprite_address + 3]
-      if sprite.on_line self.ly, sprite_height
-        index = 0
-        if !@cgb_ptr.value
-          sprites.each do |sprite_elm|
-            break if sprite.x >= sprite_elm.x
-            index += 1
-          end
-        end
-        sprites.insert index, sprite
-      end
-      break if sprites.size >= 10
-    end
-    sprites
-  end
-
-  # color idx, BG-to-OAM priority bit
-  @scanline_color_vals = Array(Tuple(UInt8, Bool)).new Display::WIDTH, {0_u8, false}
-
-  def scanline
-    @current_window_line = 0 if self.ly == 0
-    should_increment_window_line = false
-    bg_palette = palette_to_array @bgp
-    window_map = window_tile_map == 0_u8 ? 0x1800 : 0x1C00       # 0x9800 : 0x9C00
-    background_map = bg_tile_map == 0_u8 ? 0x1800 : 0x1C00       # 0x9800 : 0x9C00
-    tile_data_table = bg_window_tile_data == 0 ? 0x1000 : 0x0000 # 0x9000 : 0x8000
-    tile_row_window = @current_window_line % 8
-    tile_row = (self.ly.to_u16 + @scy) % 8
-    (0...Display::WIDTH).each do |x|
-      if window_enabled? && self.ly >= @wy && x + 7 >= @wx
-        should_increment_window_line = true
-        tile_num_addr = window_map + ((x + 7 - @wx) // 8) + ((@current_window_line // 8) * 32)
-        tile_num = @vram[0][tile_num_addr]
-        tile_num = tile_num.to_i8! if bg_window_tile_data == 0
-        tile_ptr = tile_data_table + 16 * tile_num
-        bank_num = @cgb_ptr.value ? (@vram[1][tile_num_addr] & 0b00001000) >> 3 : 0
-        if @cgb_ptr.value && @vram[1][tile_num_addr] & 0b01000000 > 0
-          byte_1 = @vram[bank_num][tile_ptr + (7 - tile_row) * 2]
-          byte_2 = @vram[bank_num][tile_ptr + (7 - tile_row) * 2 + 1]
-        else
-          byte_1 = @vram[bank_num][tile_ptr + tile_row_window * 2]
-          byte_2 = @vram[bank_num][tile_ptr + tile_row_window * 2 + 1]
-        end
-        if @cgb_ptr.value && @vram[1][tile_num_addr] & 0b00100000 > 0
-          lsb = (byte_1 >> ((x + 7 - @wx) % 8)) & 0x1
-          msb = (byte_2 >> ((x + 7 - @wx) % 8)) & 0x1
-        else
-          lsb = (byte_1 >> (7 - ((x + 7 - @wx) % 8))) & 0x1
-          msb = (byte_2 >> (7 - ((x + 7 - @wx) % 8))) & 0x1
-        end
-        color = (msb << 1) | lsb
-        @scanline_color_vals[x] = {color, @vram[1][tile_num_addr] & 0x80 > 0}
-        if @cgb_ptr.value
-          @framebuffer[Display::WIDTH * self.ly + x] = @palettes[@vram[1][tile_num_addr] & 0b111][color].convert_from_cgb
-        else
-          @framebuffer[Display::WIDTH * self.ly + x] = @palettes[0][bg_palette[color]].convert_from_cgb
-        end
-      elsif bg_display? || @cgb_ptr.value
-        tile_num_addr = background_map + (((x + @scx) // 8) % 32) + ((((self.ly.to_u16 + @scy) // 8) * 32) % (32 * 32))
-        tile_num = @vram[0][tile_num_addr]
-        tile_num = tile_num.to_i8! if bg_window_tile_data == 0
-        tile_ptr = tile_data_table + 16 * tile_num
-        bank_num = @cgb_ptr.value ? (@vram[1][tile_num_addr] & 0b00001000) >> 3 : 0
-        if @cgb_ptr.value && @vram[1][tile_num_addr] & 0b01000000 > 0
-          byte_1 = @vram[bank_num][tile_ptr + (7 - tile_row) * 2]
-          byte_2 = @vram[bank_num][tile_ptr + (7 - tile_row) * 2 + 1]
-        else
-          byte_1 = @vram[bank_num][tile_ptr + tile_row * 2]
-          byte_2 = @vram[bank_num][tile_ptr + tile_row * 2 + 1]
-        end
-        if @cgb_ptr.value && @vram[1][tile_num_addr] & 0b00100000 > 0
-          lsb = (byte_1 >> ((x + @scx) % 8)) & 0x1
-          msb = (byte_2 >> ((x + @scx) % 8)) & 0x1
-        else
-          lsb = (byte_1 >> (7 - ((x + @scx) % 8))) & 0x1
-          msb = (byte_2 >> (7 - ((x + @scx) % 8))) & 0x1
-        end
-        color = (msb << 1) | lsb
-        @scanline_color_vals[x] = {color, @vram[1][tile_num_addr] & 0x80 > 0}
-        if @cgb_ptr.value
-          @framebuffer[Display::WIDTH * self.ly + x] = @palettes[@vram[1][tile_num_addr] & 0b111][color].convert_from_cgb
-        else
-          @framebuffer[Display::WIDTH * self.ly + x] = @palettes[0][bg_palette[color]].convert_from_cgb
-        end
-      end
-    end
-    @current_window_line += 1 if should_increment_window_line
-
-    if sprite_enabled?
-      get_sprites.each do |sprite|
-        sprite_palette = palette_to_array(sprite.dmg_palette_number == 0 ? @obp0 : @obp1)
-        bytes = sprite.bytes self.ly, sprite_height
-        (0...8).each do |col|
-          x = col + sprite.x - 8
-          next unless 0 <= x < Display::WIDTH # only render sprites on screen
-          if sprite.x_flip?
-            lsb = (@vram[@cgb_ptr.value ? sprite.bank_num : 0][bytes[0]] >> col) & 0x1
-            msb = (@vram[@cgb_ptr.value ? sprite.bank_num : 0][bytes[1]] >> col) & 0x1
-          else
-            lsb = (@vram[@cgb_ptr.value ? sprite.bank_num : 0][bytes[0]] >> (7 - col)) & 0x1
-            msb = (@vram[@cgb_ptr.value ? sprite.bank_num : 0][bytes[1]] >> (7 - col)) & 0x1
-          end
-          color = (msb << 1) | lsb
-          if color > 0 # color 0 is transparent
-            if @cgb_ptr.value
-              # if !bg_display, then objects are always on top in cgb mode
-              # objects are always on top of bg/window color 0
-              # objects are on top of bg/window colors 1-3 if bg_priority and object priority are both unset
-              if !bg_display? || @scanline_color_vals[x][0] == 0 || (!@scanline_color_vals[x][1] && sprite.priority == 0)
-                @framebuffer[Display::WIDTH * self.ly + x] = @obj_palettes[sprite.cgb_palette_number][color].convert_from_cgb
-              end
-            else
-              if sprite.priority == 0 || @scanline_color_vals[x][0] == 0
-                @framebuffer[Display::WIDTH * self.ly + x] = @obj_palettes[0][sprite_palette[color]].convert_from_cgb
-              end
-            end
-          end
-        end
-      end
-    end
-  end
-
   @old_stat_flag = false
 
   # handle stat interrupts
@@ -279,48 +165,109 @@ class PPU
     @old_stat_flag = stat_flag
   end
 
+  @fetch_counter = 0
+  @x_pos = 0
+
+  @tile_num : UInt8 = 0x00
+  @tile_data_low : UInt8 = 0x00
+  @tile_data_high : UInt8 = 0x00
+
   # tick ppu forward by specified number of cycles
   def tick(cycles : Int) : Nil
-    @counter += cycles
     if lcd_enabled?
-      if self.mode_flag == 2 # oam search
-        if @counter >= 80    # end of oam search reached
-          @counter -= 80     # reset counter, saving extra cycles
-          self.mode_flag = 3 # switch to drawing
-        end
-      elsif self.mode_flag == 3 # drawing
-        if @counter >= 172      # end of drawing reached
-          @counter -= 172       # reset counter, saving extra cycles
-          self.mode_flag = 0    # switch to hblank
-          scanline              # store scanline data
-        end
-      elsif self.mode_flag == 0 # hblank
-        if @counter >= 204      # end of hblank reached
-          @counter -= 204       # reset counter, saving extra cycles
-          self.ly += 1
-          if self.ly == Display::HEIGHT # final row of screen complete
-            self.mode_flag = 1          # switch to vblank
-            @interrupts.vblank_interrupt = true
-            @display.draw @framebuffer # render at vblank
-          else
-            self.mode_flag = 2 # switch to oam search
+      cycles.times do
+        case self.mode_flag
+        when 2 # OAM search
+          if @cycle_counter == 80
+            self.mode_flag = 3
+            @fifo.clear
+            @x_pos = 0
           end
-        end
-      elsif self.mode_flag == 1 # vblank
-        if @counter >= 456      # end of line reached
-          @counter -= 456       # reset counter, saving extra cycles
-          self.ly += 1 if self.ly != 0
-          if self.ly == 0      # end of vblank reached (ly has already shortcut to 0)
-            self.mode_flag = 2 # switch to oam search
+        when 3 # drawing
+          case @fetch_counter
+          when 0, 1 # fetching tile number
+            if @fetch_counter == 0
+              background_map = bg_tile_map == 0 ? 0x1800 : 0x1C00 # 0x9800 : 0x9C00
+              tile_num_offset = @x_pos // 8 + (@ly.to_u16 // 8) * 32
+              @tile_num = @vram[0][background_map + tile_num_offset]
+            end
+          when 2, 3 # fetching low tile data
+            if @fetch_counter == 2
+              if bg_window_tile_data > 0
+                tile_num = @tile_num
+                tile_data_table = 0x0000 # 0x8000
+              else
+                tile_num = @tile_num.to_i8!
+                tile_data_table = 0x1000 # 0x9000
+              end
+              # tile_num = bg_window_tile_data > 0 ? @tile_num : @tile_num.to_i8!
+              tile_ptr = tile_data_table + 16 * tile_num
+              @tile_data_low = @vram[0][tile_ptr + (@ly % 8) * 2]
+            end
+          when 4, 5 # fetching high tile data
+            if @fetch_counter == 4
+              if bg_window_tile_data > 0
+                tile_num = @tile_num
+                tile_data_table = 0x0000 # 0x8000
+              else
+                tile_num = @tile_num.to_i8!
+                tile_data_table = 0x1000 # 0x9000
+              end
+              # tile_num = bg_window_tile_data > 0 ? @tile_num : @tile_num.to_i8!
+              tile_ptr = tile_data_table + 16 * tile_num
+              @tile_data_high = @vram[0][tile_ptr + (@ly % 8) * 2 + 1]
+            end
+          else # attempt pushing 8 pixels into fifo
+            if @fifo.size == 0
+              # puts "low: #{hex_str @tile_data_low}, high: #{hex_str @tile_data_high}"
+              8.times do |col|
+                lsb = (@tile_data_low >> (7 - col)) & 0x1
+                msb = (@tile_data_high >> (7 - col)) & 0x1
+                color = (msb << 1) | lsb
+                @fifo.push Pixel.new(color, 0, 0, 0)
+                @x_pos += 1
+              end
+              @fetch_counter = -1
+            end
+            if @fifo.size > 0
+              palette = palette_to_array @bgp
+              pixel = @fifo.shift
+              @framebuffer[@framebuffer_pos] = @palettes[0][palette[pixel.color]].convert_from_cgb
+              @framebuffer_pos += 1
+              if @framebuffer_pos % Display::WIDTH == 0
+                self.mode_flag = 0
+              end
+            end
           end
+          @fetch_counter += 1
+        when 0 # hblank
+          if @cycle_counter == 456
+            @cycle_counter = 0
+            self.ly += 1
+            if self.ly == Display::HEIGHT # final row of screen complete
+              self.mode_flag = 1          # switch to vblank
+              @interrupts.vblank_interrupt = true
+              @display.draw @framebuffer # render at vblank
+              @framebuffer_pos = 0
+            else
+              self.mode_flag = 2 # switch to oam search
+            end
+          end
+        when 1 # vblank
+          if @cycle_counter == 456
+            @cycle_counter = 0
+            self.ly += 1 if self.ly != 0
+            if self.ly == 0      # end of vblank reached (ly has already shortcut to 0)
+              self.mode_flag = 2 # switch to oam search
+            end
+          end
+          self.ly = 0 if self.ly == 153 && @cycle_counter > 4 # shortcut ly to from 153 to 0 after 4 cycles
         end
-        self.ly = 0 if self.ly == 153 && @counter > 4 # shortcut ly to from 153 to 0 after 4 cycles
-      else
-        raise "Invalid mode #{self.mode_flag}"
+        @cycle_counter += 1
+        handle_stat_interrupt
       end
-      handle_stat_interrupt
     else                 # lcd is disabled
-      @counter = 0       # reset cycle counter
+      @cycle_counter = 0 # reset cycle counter
       self.mode_flag = 0 # reset to mode 0
       self.ly = 0        # reset ly
     end
