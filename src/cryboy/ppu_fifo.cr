0aa61f4782c3f34b4a3dd12c7002b28af9afbba8
Matthew Berry <me@mattrb.com>
2020-07-30 08:15:53 -0700

    clean up fetch stages

--- src/cryboy/ppu_fifo.cr
+++ src/cryboy/ppu_fifo.cr
@@ -172,6 +172,21 @@ class PPU
   @tile_data_low : UInt8 = 0x00
   @tile_data_high : UInt8 = 0x00
 
+  enum FetchStage
+    GET_TILE
+    GET_TILE_DATA_LOW
+    GET_TILE_DATA_HIGH
+    PUSH_PIXEL
+    SLEEP
+  end
+
+  FETCHER_ORDER = [
+    FetchStage::SLEEP, FetchStage::GET_TILE,
+    FetchStage::SLEEP, FetchStage::GET_TILE_DATA_LOW,
+    FetchStage::SLEEP, FetchStage::GET_TILE_DATA_HIGH,
+    FetchStage::PUSH_PIXEL,
+  ]
+
   # tick ppu forward by specified number of cycles
   def tick(cycles : Int) : Nil
     if lcd_enabled?
@@ -186,43 +201,40 @@ class PPU
             @fetch_counter = 0
           end
         when 3 # drawing
-          case @fetch_counter
-          when 0, 1 # fetching tile number
-            if @fetch_counter == 0
-              background_map = bg_tile_map == 0 ? 0x1800 : 0x1C00 # 0x9800 : 0x9C00
-              tile_num_offset = ((@fetcher_x + (@scx // 8)) % 32) + ((((@ly.to_u16 + @scy) // 8) * 32) % (32 * 32))
-              @tile_num = @vram[0][background_map + tile_num_offset]
+          case FETCHER_ORDER[@fetch_counter]
+          in FetchStage::GET_TILE
+            background_map = bg_tile_map == 0 ? 0x1800 : 0x1C00 # 0x9800 : 0x9C00
+            tile_num_offset = ((@fetcher_x + (@scx // 8)) % 32) + ((((@ly.to_u16 + @scy) // 8) * 32) % (32 * 32))
+            @tile_num = @vram[0][background_map + tile_num_offset]
+            @fetch_counter += 1
+          in FetchStage::GET_TILE_DATA_LOW
+            if bg_window_tile_data > 0
+              tile_num = @tile_num
+              tile_data_table = 0x0000 # 0x8000
+            else
+              tile_num = @tile_num.to_i8!
+              tile_data_table = 0x1000 # 0x9000
             end
-          when 2, 3 # fetching low tile data
-            if @fetch_counter == 2
-              if bg_window_tile_data > 0
-                tile_num = @tile_num
-                tile_data_table = 0x0000 # 0x8000
-              else
-                tile_num = @tile_num.to_i8!
-                tile_data_table = 0x1000 # 0x9000
-              end
-              # tile_num = bg_window_tile_data > 0 ? @tile_num : @tile_num.to_i8!
-              tile_ptr = tile_data_table + 16 * tile_num
-              @tile_data_low = @vram[0][tile_ptr + ((@ly.to_u16 + @scy) % 8) * 2]
+            # tile_num = bg_window_tile_data > 0 ? @tile_num : @tile_num.to_i8!
+            tile_ptr = tile_data_table + 16 * tile_num
+            @tile_data_low = @vram[0][tile_ptr + ((@ly.to_u16 + @scy) % 8) * 2]
+            @fetch_counter += 1
+          in FetchStage::GET_TILE_DATA_HIGH
+            if bg_window_tile_data > 0
+              tile_num = @tile_num
+              tile_data_table = 0x0000 # 0x8000
+            else
+              tile_num = @tile_num.to_i8!
+              tile_data_table = 0x1000 # 0x9000
             end
-          when 4, 5 # fetching high tile data
-            if @fetch_counter == 4
-              if bg_window_tile_data > 0
-                tile_num = @tile_num
-                tile_data_table = 0x0000 # 0x8000
-              else
-                tile_num = @tile_num.to_i8!
-                tile_data_table = 0x1000 # 0x9000
-              end
-              # tile_num = bg_window_tile_data > 0 ? @tile_num : @tile_num.to_i8!
-              tile_ptr = tile_data_table + 16 * tile_num
-              @tile_data_high = @vram[0][tile_ptr + ((@ly.to_u16 + @scy) % 8) * 2 + 1]
-            end
+            # tile_num = bg_window_tile_data > 0 ? @tile_num : @tile_num.to_i8!
+            tile_ptr = tile_data_table + 16 * tile_num
+            @tile_data_high = @vram[0][tile_ptr + ((@ly.to_u16 + @scy) % 8) * 2 + 1]
+            @fetch_counter += 1
             if @cycle_counter == 86
-              @fetch_counter = -1 # drop first tile
+              @fetch_counter = 0 # drop first tile
             end
-          else # attempt pushing 8 pixels into fifo
+          in FetchStage::PUSH_PIXEL
             if @fifo.size == 0
               @fetcher_x += 1
               8.times do |col|
@@ -231,9 +243,12 @@ class PPU
                 color = (msb << 1) | lsb
                 @fifo.push Pixel.new(color, 0, 0, 0)
               end
-              @fetch_counter = -1 # reset fetcher phase
+              @fetch_counter += 1
             end
+          in FetchStage::SLEEP
+            @fetch_counter += 1
           end
+          @fetch_counter %= FETCHER_ORDER.size
           if @fifo.size > 0
             palette = palette_to_array @bgp
             pixel = @fifo.shift
@@ -246,7 +261,6 @@ class PPU
               self.mode_flag = 0
             end
           end
-          @fetch_counter += 1
         when 0 # hblank
           if @cycle_counter == 456
             @cycle_counter = 0
